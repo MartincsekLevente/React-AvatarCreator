d1f6a7e9fd63dfd7b9cc546ba34458f41ed3e6fa
Martincsek Levente <martincsekl@gmail.com>
2025-02-12 15:50:44 +0100

    WIP

--- src/Components/AvatarCardEditor/AvatarCardEditor.tsx
+++ src/Components/AvatarCardEditor/AvatarCardEditor.tsx
@@ -1,9 +1,14 @@
 import './AvatarCardEditor.css';
+import { Image } from '@mantine/core';
 
 export default function AvatarCardEditor() {
     return (
         <div className={"avatar-card-editor-container"}>
-            <div>AvatarCardEditor</div>
+            <div className={"avatar-card"}>
+                <div className={"avatar-image-container"}>
+                    <div>Image placeholder</div>
+                </div>
+            </div>
         </div>
     );
 }
